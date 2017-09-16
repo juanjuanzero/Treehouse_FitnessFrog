aed837808e5ae1fa6d1f0b2de9361c04647aca62
Juan E Afable <juanjuanzerojuan@gmail.com>
2017-09-16 06:21:06 -0400

    updated the add entry view to use the html helper methods

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -52,13 +52,8 @@ namespace Treehouse.FitnessFrog.Controllers
             //string date = Request.Form["Date"]; instead of doing it like this you can specify a parameter in the method call. see up inside AddPost()
             
             //updated the parameter values in the method from string to appropriate parameters. IntensityLevel is an enum defined else where.
-            ViewBag.Date = ModelState["Date"].Value.AttemptedValue; //doing this lets us carry the value entered but would also keep what you entered.
-            ViewBag.ActivityID = ModelState["ActivityId"].Value.AttemptedValue;
-            ViewBag.Duration = ModelState["Duration"].Value.AttemptedValue;
-            ViewBag.Intensity = ModelState["Intensity"].Value.AttemptedValue;
-            ViewBag.Exclude = ModelState["Exclude"].Value.AttemptedValue;
-            ViewBag.Notes = ModelState["Notes"].Value.AttemptedValue;
-            
+            //ViewBag.Date = ModelState["Date"].Value.AttemptedValue; //doing this lets us carry the value entered but would also keep what you entered.
+
             return View();
         }
 
