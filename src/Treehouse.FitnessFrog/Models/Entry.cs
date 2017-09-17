ccea265043651aa3c5f4b954d0ed75306e79df2c
Juan E Afable <juanjuanzerojuan@gmail.com>
2017-09-17 15:10:37 -0400

    added validation rules using data annotations.

--- src/Treehouse.FitnessFrog/Models/Entry.cs
+++ src/Treehouse.FitnessFrog/Models/Entry.cs
@@ -88,7 +88,8 @@ namespace Treehouse.FitnessFrog.Models
 
         /// <summary>
         /// The notes for the entry.
-        /// </summary>
+        /// </summary> added data annotations
+        [MaxLength(200, ErrorMessage ="The Notes field cannot be longer than 200 characters")]
         public string Notes { get; set; }
     }
 }
